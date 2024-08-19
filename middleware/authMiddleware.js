e71e8c8a612a80ed8fa3a1669de2cc5a23d259a4
EdelweissPirate <joshmarkevans@gmail.com>
2024-08-19 08:34:29 -0500

    updating app

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,9 +1,9 @@
 const asyncHandler = require('express-async-handler')
 
 const protect = asyncHandler(async (req, res, next) => {
-    const referer = req.headers.referer;
+    const referer = req.headers?.referer;
 
-    if(!referer || !referer.includes("ec2-18-133-140-147")){
+    if(!referer || !referer.includes("ec2-35-178-195-77")){
         res.status(403).send("Not authorised")
     } else {
         next();
--- server.js
+++ server.js
@@ -1,77 +1,45 @@
+const https = require("https");
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
 const app = express();
 
-const port = 3000;
+const port = 3001;
 
 const protect = require("./middleware/authMiddleware");
 const { errorHandler } = require("./middleware/errorMiddleware");
 
 app.use(errorHandler);
 app.use(express.static(path.join(__dirname, "/public")));
-app.use(express.static(path.join(__dirname, "/public/ARP-L_LESSON_2_IMPLEMENTATION_PART_1")));
+app.use(express.static(path.join(__dirname, "/public/English")));
+app.use(express.static(path.join(__dirname, "/public/English/VACATI")));
 
-app.get("/api", protect, (req, res) => {
+app.get("/api", (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
         .send("Welcome to the Mobius API");
 })
 
-app.get("/api/getVideo", protect, (req, res) => {
-    // let range = req.headers.range;
-    
-    // console.log("range: ", range);
+app.get("/api/getVideo", (req, res) => {
+    const videoName = req.query.videoName;
 
-    // if(!range || range === undefined) {
-    //     range = "bytes=0-";
-    // }
-
-    let params = (new URL(req.headers.referer + req.url)).searchParams;
-
-    const urlParams = new URLSearchParams(params);
-    const videoName = urlParams.get("videoName")
     const filename = __dirname + "/public/" + videoName + ".mp4";
 
     res.setHeader("Access-Control-Allow-Origin", "*")
         .status(200)
         .sendFile(filename);
-    
-    ////////
-    // const videoSize = fs.statSync(filename).size;
-
-    // // Set the response headers
-    // res.writeHead(200, {
-    //     "Access-Control-Allow-Origin": "*",
-    //     "Content-Type": "video/mp4",
-    //     "Content-Length": videoSize,
-    // });
-
-    // // Create a read stream and pipe it to the response object in chunks. Sent in 512kb chunks
-    // const videoStream = fs.createReadStream(filename, { highWaterMark: 512 * 1024 });
-    // videoStream.pipe(res);
-    ///////////
-
-
-    // const videoSize = fs.statSync(filename).size;
-    // const CHUNK_SIZE = 10 ** 6;
-    // const start = Number(range.replace(/\D/g, ""));
-    // const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-    // const contentLength = end - start + 1;
-    // const headers = {
-    //     "Access-Control-Allow-Origin": "*",
-    //     "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    //     "Accept-Ranges": "bytes",
-    //     "Content-Length": contentLength,
-    //     "Content-Type": "video/mp4",
-    // };
-
-    // // console.log(videoSize, CHUNK_SIZE, start, end, contentLength, headers);
-
-    // res.writeHead(206, headers);
-    // const videoStream = fs.createReadStream(filename, { start, end });
-    // // console.log("Video stream created successfully: ", videoStream);
-    // videoStream.pipe(res);
 })
 
-app.listen(port, () => {console.log(`listening at port: ${port}`)});
+// app.listen(port, () => {console.log(`listening at port: ${port}`)});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync('./ssl/server.key'),
+      cert: fs.readFileSync('./ssl/server.cer')
+    }, 
+    app
+  )
+  .listen(port, ()=>{
+    console.log(`server is runing at port ${port}`)
+  });
